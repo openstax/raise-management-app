70330caa313b323eeb09d5ddb5b227905d0dc65a
Ripal Nathuji <rnathuji@neoteklabs.com>
2021-10-26 19:11:08 -0500

    Pull Cognito settings from environment for FE

--- frontend/src/lib/env.ts
+++ frontend/src/lib/env.ts
@@ -1,13 +1,9 @@
 const IS_LOCAL = window.location.hostname === 'localhost'
 const API_PATH = IS_LOCAL ? 'http://localhost:8000' : '/api'
 
-// NOTE: Just adding the dev cognito details directly for now. Later when we
-// have multiple pools for different environments, we can use a mechanism like
-// Vite env to set different values.
 export const ENV = {
-  AWS_REGION: 'us-east-1',
-  COGNITO_USER_POOL_ID: 'us-east-1_bpBEyr8Xh',
-  COGNITO_CLIENT_ID: '65g2uk6gdjjlbn3qasvb09qff6',
+  COGNITO_USER_POOL_ID: import.meta.env.VITE_COGNITO_USER_POOL_ID as string,
+  COGNITO_CLIENT_ID: import.meta.env.VITE_COGNITO_CLIENT_ID as string,
   API_PATH: API_PATH
 }
 
